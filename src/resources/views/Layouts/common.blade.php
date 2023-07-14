73822d9dba5a79a5ededd7bb4eabd499e260ab60
The.First.Step <46297678+AnijaaaPan@users.noreply.github.com>
2023-07-14 15:28:50 +0900

    update

--- src/resources/views/Layouts/common.blade.php
+++ src/resources/views/Layouts/common.blade.php
@@ -4,6 +4,7 @@
 <head>
   @include('Parts.head')
 
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
   @vite(['resources/scss/app.scss'])
   @yield('add-scss')
 </head>
